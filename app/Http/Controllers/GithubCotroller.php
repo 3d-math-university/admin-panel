28d6c142f69e2ad7e1e9f05c0ba579bae8a9749d
rublev <rublyov.andrey@outlook.com>
2019-02-28 04:55:25 +0300

    small fix for github api

--- app/Http/Controllers/GithubCotroller.php
+++ app/Http/Controllers/GithubCotroller.php
@@ -9,6 +9,18 @@ class GithubController extends Controller
 {
     public function deploy(Request $request)
     {
-        echo 1;
+        $githubPayload = $request->getContent();
+        $githubHash = $request->header('X-Hub-Signature');
+
+        $localToken = config('app.deploy_secret');
+        $localHash = 'sha1=' . hash_hmac('sha1', $githubPayload, $localToken, false);
+
+//        if (hash_equals($githubHash, $localHash)) {
+            $root_path = base_path();
+            $process = new Process('cd ' . $root_path . '; ./deploy.sh');
+            $process->run(function ($type, $buffer) {
+                echo $buffer;
+            });
+//        }
     }
 }
